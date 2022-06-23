834c16679890dbd436104ae79c79b883ac4106e8
Arian <75135088+avrancic@users.noreply.github.com>
2022-06-23 13:36:21 +0200

    Update

---
+++ src/views/Redomat.vue
@@ -0,0 +1,16 @@
+<template>
+<div style = "width:800px; margin: 0  auto;">
+        <p>
+          Adresa: Muntriljska 2, Pazin, 52 000<br>
+          Telefon: 052/538 139<br>
+          Telefaks: 052/538 121<br>
+          e-mail : pp.pazin@mup.hr<br>
+        </p>
+        <p>
+          052/538 134   osobne iskaznice, putovnice, prebivalište i boravište<br>
+          052/538 135   stranci i državljanstvo<br>
+          052/538 135   prometne isprave (registracija vozila)<br>
+          052/538 162   oružje i prometne isprave(vozači/vozačke dozvole)<br>
+        </p>
+    </div>
+    </template>
