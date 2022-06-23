fc2fe8035388ff4774a7cd8b409e4965b138c6d8
Arian <75135088+avrancic@users.noreply.github.com>
2022-06-23 23:25:04 +0200

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCvJLZewqkgs5XT9l-F720ckD9AqoD2csQ",
@@ -10,11 +8,9 @@ const firebaseConfig = {
     storageBucket: "infomup-80dd3.appspot.com",
     messagingSenderId: "227836827947",
     appId: "1:227836827947:web:4acaa3e5bc99e4d71b0d6e"
-  };
-  
-firebase.initializeApp(firebaseConfig);
+  };  
 
-let db = firebase.firestore();
-let storage = firebase.storage();
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
 
-export { firebase, db, storage };
+export { auth };
